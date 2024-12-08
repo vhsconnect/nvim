3c42f877895db78579fa3cf7019a7d03b510ea5e
vhsconnect <90valentin@gmail.com>
2024-12-08 16:27:28 +0100

    update avante model

--- modules/avante/default.nix
+++ modules/avante/default.nix
@@ -40,7 +40,7 @@ in
               provider = "claude",
               claude = {
                 endpoint = "https://api.anthropic.com",
-                model = "claude-3-5-sonnet-20240620",
+                model = "claude-3-5-sonnet-20241022",
                 temperature = 0,
                 max_tokens = 4096,
               },
