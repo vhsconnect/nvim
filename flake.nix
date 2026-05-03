b33a046cdab557c16fcc4665f68869247fee05e5
vhsconnect <90valentin@gmail.com>
2026-05-03 22:52:35 +0200

    updates 2026/5

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
     plugin-nvim-dap-virtual-text.flake = false;
 
     # Copying/Registers
-    plugin-registers.url = "github:tversteeg/registers.nvim";
+    plugin-registers.url = "git+https://codeberg.org/fosk/registers.nvim.git";
     plugin-registers.flake = false;
 
     plugin-nvim-neoclip.url = "github:AckslD/nvim-neoclip.lua";
@@ -513,7 +513,7 @@
                 treesitter.enable = true;
               };
               gleam = {
-                enable = true;
+                enable = false;
                 format.enable = true;
                 treesitter.enable = true;
               };
@@ -616,13 +616,15 @@
             luaConfigRC = {
               a = "${builtins.readFile ./rc.lua}";
               b = "${builtins.readFile ./cheat_sheet.lua}";
-              c =
+              c = "${builtins.readFile ./dynamic_path.lua}";
+              d =
                 let
                   luaFolder = builtins.path {
                     path = ./extra;
                     name = "extra-lua";
                   };
                 in
+                # lua
                 ''
                   vim.opt.runtimepath:append("${luaFolder}")
                   require ("flutter").setup()
