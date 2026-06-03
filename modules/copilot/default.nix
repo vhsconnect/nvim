078a18afd7a0c5bc86ece800183c68ad5f69c035
vhsconnect <90valentin@gmail.com>
2026-06-03 23:56:04 +0200

    copilot

--- modules/copilot/default.nix
+++ modules/copilot/default.nix
@@ -28,6 +28,13 @@ in
         ''
           vim.g.copilot_enabled = false
           require("CopilotChat").setup{}
+
+          vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+            expr = true,
+            replace_keycodes = false
+          })
+
+          vim.g.copilot_no_tab_map = true
         '';
   };
 }
