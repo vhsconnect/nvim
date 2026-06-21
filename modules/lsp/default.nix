c760aad5b763c36a026593e3d2200a4667eac294
vhsconnect <90valentin@gmail.com>
2026-06-21 20:25:36 +0200

    make format on save async

--- modules/lsp/default.nix
+++ modules/lsp/default.nix
@@ -120,7 +120,7 @@ in
               callback = function()
                 if vim.g.formatsave then
                   if client:supports_method("textDocument/formatting") then
-                    vim.lsp.buf.format({ bufnr = bufnr, filter = function(c) return c.id == client.id end })
+                    vim.lsp.buf.format({ async = true, bufnr = bufnr, filter = function(c) return c.id == client.id end })
                   end
                 end
               end
